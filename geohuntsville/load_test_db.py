0e81ec3ad5d0046f3664f3ddf2656e67ae2daad1
ealang <ealang@gmail.com>
2017-02-25 22:23:34 -0600

    Cleanup log statement

--- geohuntsville/load_test_db.py
+++ geohuntsville/load_test_db.py
@@ -5,12 +5,10 @@ from geohuntsville.geodb import GeoDb
 
 logger = logging.getLogger(__name__)
 
+
 def load_test_db():
     db = GeoDb()
     with open('data/coordinates.json') as fp:
-        x = json.load(fp)
-        logger.info("loading")
-        for data in x:
-            logger.debug(data)
+        for data in json.load(fp):
             db.insert(data['coordinate'][0], data['coordinate'][1], data['data'])
     return db
